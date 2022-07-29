04ab6792d0b798504ab9974f262277149758fa95
Yevgeniy Simonov <simonov.yevgeniy@gmail.com>
2022-07-30 00:58:04 +0800

    feat: added comments

--- leetcode/longest_common_prefix.cpp
+++ leetcode/longest_common_prefix.cpp
@@ -4,6 +4,25 @@
 
 using namespace std;
 
+/*
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+*/
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
