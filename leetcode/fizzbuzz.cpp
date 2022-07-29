8f81c59b57d4ccd1184194c6698db36b49cf186d
Yevgeniy Simonov <simonov.yevgeniy@gmail.com>
2022-07-29 23:41:49 +0800

    feat: roman_to_int leetcode

---
+++ leetcode/fizzbuzz.cpp
@@ -0,0 +1,72 @@
+/*
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
+
+Constraints:
+
+1 <= n <= 10^4
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+using std::cout;
+using std::endl;
+
+class Solution {
+public:
+    Solution() = default;
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+        result.reserve(n);
+        int k = min(n+1, 10001);
+        for(int i = 1; i < k; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                result.emplace_back("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.emplace_back("Fizz");
+            } else if (i % 5 == 0) {
+                result.emplace_back("Buzz");
+            } else {
+                auto temp = to_string(i);
+                cout << temp << endl;
+                result.emplace_back(temp);
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+
+    Solution sol;
+    auto result = sol.fizzBuzz(10);
+
+    for (auto &it : result) {
+        cout << it << '\n';
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ leetcode/roman_to_int.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+/*
+Expected results:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+Constraints:
+
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+*/
+
+class Solution {
+public:
+    int romanToInt(string str) {
+        // firstly check string length
+        auto str_len = str.length();
+        if (str_len == 0) 
+            return 0;
+        unordered_map<char, int> dict = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        unordered_map<string, int> dict_combinations = {
+            {"IV", 4},
+            {"IX", 9},
+            {"XL", 40},
+            {"XC", 90},
+            {"CD", 400},
+            {"CM", 900}
+        };
+        if (str_len == 1) {
+            return dict[str.at(0)];
+        } else {
+            vector<int> total_values;
+            int current_idx = 0;
+            total_values.emplace_back(dict[str.at(0)]);
+            for (int i = 1; i < str_len; i++) {
+                // check if prev + curr make a valid combination
+                std::string char_comb {str.at(i-1), str.at(i)};
+                // look for char_comb in dict_combinations
+                auto it = dict_combinations.find(char_comb);
+                // if combination was found, update previous value of the container
+                if (it != dict_combinations.end()) {
+                    total_values[current_idx] = dict_combinations[char_comb];
+                } else {
+                    // increment index
+                    current_idx += 1;
+                    // append new value for total container
+                    total_values.emplace_back(dict[str.at(i)]);
+                }
+            }
+            // accumulate all values (sum)
+            int result = std::accumulate(total_values.begin(), total_values.end(), 0);
+            return result;
+        }
+    }
+};
+
+int main() {
+    auto sol = Solution();
+    auto r1 = sol.romanToInt("III");
+    auto r2 = sol.romanToInt("LVIII");
+    auto r3 = sol.romanToInt("MCMXCIV");
+    cout << r1 << " " << r2 << " " << r3 << endl;
+    return EXIT_SUCCESS;
+}
