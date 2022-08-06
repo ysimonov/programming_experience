57eb848a7d28f4af25ce6169650e5b43d2c57aa5
Yevgeniy Simonov <simonov.yevgeniy@gmail.com>
2022-08-06 21:41:22 +0800

    test: testing eigen library

---
+++ eigen/matops.cpp
@@ -0,0 +1,35 @@
+#include <eigen3/Eigen/Dense>
+#include <iostream>
+
+using namespace Eigen;
+
+int main() {
+
+    Matrix<double, 3, 3> a;
+    a <<    1.0f, 2.0f, 3.0f,
+            4.0f, 5.0f, 6.0f,
+            7.0f, 8.0f, 9.0f;
+
+    MatrixXd b = MatrixXd::Random(3, 3);
+
+    std::cout << "Matrix a: \n";
+    std::cout << a << "\n\n";
+
+    std::cout << "Matrix b: \n";
+    std::cout << b << "\n\n";
+
+    auto sum = a + b;
+    auto diff = a - b;
+    auto prod = a * b;
+
+    std::cout << "Matrix a + b: \n";
+    std::cout << sum << "\n\n";
+
+    std::cout << "Matrix a - b: \n";
+    std::cout << diff << "\n\n";
+
+    std::cout << "Matrix a * b: \n";
+    std::cout << prod << "\n\n";
+    
+    return EXIT_SUCCESS;
+}
