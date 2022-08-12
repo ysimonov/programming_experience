175832a34be87d564d7273d34f8b1efe2e162913
Yevgeniy Simonov <simonov.yevgeniy@gmail.com>
2022-08-13 03:24:53 +0800

    feat: server template

---
+++ cpp_general/sockets/server.cpp
@@ -0,0 +1,160 @@
+#include <cstring> // sizeof()
+#include <iostream>
+#include <string>
+
+// socket(), getaddinfo(), friends
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <unistd.h> // close()
+
+void free_addrmem(addrinfo* ptr_res, addrinfo* ptr_p) {
+    freeaddrinfo(ptr_res);
+    freeaddrinfo(ptr_p);
+}
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 2) {
+        std::cerr << "Specify port number" << std::endl;
+        return -1;
+    }
+
+    const auto& port_num = argv[1];
+    const static uint64_t num_conn = 8;
+
+    addrinfo hints;
+    addrinfo* ptr_res;  // hold
+    addrinfo* ptr_p;    // iterate
+
+    memset(&hints, 0, sizeof(hints));
+
+    // man socket
+    hints.ai_family = AF_UNSPEC;    // don't specify IP version to use
+    hints.ai_socktype = SOCK_STREAM;    // SOCK_STREAM is TCP, SOCK_DGRAM is UDP
+    hints.ai_flags = AI_PASSIVE;
+
+    // man getaddrinfo
+    int get_addr = getaddrinfo(nullptr, port_num, &hints, &ptr_res);
+    if (get_addr != 0) {
+        std::cerr << gai_strerror(get_addr) << std::endl;
+        free_addrmem(ptr_res, ptr_p);
+        return -2;
+    }
+
+    std::cout << "Finding address ..." << std::endl;
+
+    uint32_t num_of_addr = 0;
+    char ip_str[INET6_ADDRSTRLEN];  // both ipv4/6 can be stored in the variable
+
+    // getaddinfo() gives list of addresses that can be iterated over
+    // then bind to available socket address
+    ptr_p = ptr_res;
+    while (ptr_p != nullptr) {
+        void* ptr_addr;
+        std::string ip_ver;
+
+        // if ipv4
+        if (ptr_p->ai_family == AF_INET) {
+            ip_ver = "IPv4";
+            sockaddr_in* ipv4 = reinterpret_cast<sockaddr_in*>(ptr_p->ai_addr);
+            ptr_addr = &(ipv4->sin_addr);
+            ++num_of_addr;
+        } 
+
+        // if ipv6
+        else {
+            ip_ver = "IPv6";
+            sockaddr_in6* ipv6 = reinterpret_cast<sockaddr_in6*>(ptr_p->ai_addr);
+            ptr_addr = &(ipv6->sin6_addr);
+            ++num_of_addr;
+        }
+
+        // convert addresses from binary to text
+        inet_ntop(ptr_p->ai_family, ptr_addr, ip_str, sizeof(ip_str));
+        std::cout << "(" << num_of_addr << ") " << ip_ver << ": " << ip_str << std::endl;
+
+        // move to the next addr
+        ptr_p = ptr_p->ai_next;
+    }
+
+    // if no addr found
+    if (!num_of_addr) {
+        std::cout << "Found no valid host address\n";
+        free_addrmem(ptr_res, ptr_p);
+        return -3;
+    }
+
+    // ask user to select an address
+    std::cout << "Enter host address number to bind with: ";
+    uint32_t choice = 0;
+
+    while (1) {
+        std::cin >> choice;
+        if (choice > (num_of_addr + 1) || choice < 1) {
+            std::cout << "Wrong choice, please enter address number again: ";
+        } else {
+            break;
+        }
+    }
+
+    ptr_p = ptr_res;
+
+    // create new socket
+    int sock_fd = socket(ptr_p->ai_family, ptr_p->ai_socktype, ptr_p->ai_protocol);
+    if (sock_fd == -1) {
+        std::cerr << "Error occured while creating socket!\n";
+        free_addrmem(ptr_res, ptr_p);
+        return -4;
+    }
+
+    // bind address to socket that was created
+    int bind_r = bind(sock_fd, ptr_p->ai_addr, ptr_p->ai_addrlen);
+    if (bind_r == -1) {
+        std::cerr << "Error while binding socket!\n";
+        close(sock_fd); // close socket
+        free_addrmem(ptr_res, ptr_p);
+        return -5;
+    }
+
+    // start listening to socket connections
+    int listen_r = listen(sock_fd, num_conn);
+    if (listen_r == -1) {
+        std::cerr << "Error while listening to the socket!\n";
+        close(sock_fd); // close socket
+        free_addrmem(ptr_res, ptr_p);
+        return -6;   
+    }
+
+    // structure to hold client's address
+    sockaddr_storage client_addr;
+    socklen_t client_addr_size = sizeof(client_addr);
+
+    const std::string response {"Hi, how are you?"};
+
+    // infinite loop to communicate with incoming connections
+    // this connection takes client connections one at a time (sequential)
+    // it is possible to create parallel connections using fork()
+    while (1) {
+
+        // socket descriptor
+        int new_fd = accept(sock_fd, (sockaddr*) &client_addr, &client_addr_size);
+
+        if (new_fd == -1) {
+            std::cerr << "Error while acception on socket\n";
+            continue;
+        }
+
+        // number of bytes sent
+        auto bytes_sent = send(new_fd, response.data(), response.length(), 0);
+        close(new_fd);
+
+    }
+
+    close(sock_fd);
+    free_addrmem(ptr_res, ptr_p);
+
+    return 0;
+}
