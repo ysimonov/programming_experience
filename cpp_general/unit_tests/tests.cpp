142f65234385ef690545697d1fe42ab3eb839492
Yevgeniy Simonov <simonov.yevgeniy@gmail.com>
2022-08-13 02:17:52 +0800

    feat: example unit tests c++

---
+++ cpp_general/unit_tests/tests.cpp
@@ -0,0 +1,21 @@
+#include "what_to_test.cpp"
+#include <gtest/gtest.h>
+
+TEST(SquareRootTest, PositiveNos) {
+    ASSERT_EQ(6, squareRoot(36.0));
+    ASSERT_EQ(18.0, squareRoot(324.0));
+    ASSERT_EQ(0, squareRoot(0.0));
+}
+
+TEST(SquareRootTest, NegativeNos) {
+    EXPECT_EQ(-1.0, squareRoot(1.0));
+}
+
+TEST(FactorialTest, PositiveNos) {
+    ASSERT_EQ(3628800, factorial(10));
+}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ cpp_general/unit_tests/what_to_test.cpp
@@ -0,0 +1,25 @@
+#include <math.h>
+#include <stdexcept>
+
+double squareRoot(const double a) {
+    double b = sqrt(a);
+    return b;
+}
+
+/* 
+ * function that calculates factorial of a number and 
+ * throws invalid argument error if negative number is given
+*/
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else if (n > 1) {
+        int prod = 1;
+        for (int i = 2; i <= n; i++) {
+            prod *= i;
+        }
+        return prod;
+    } else {
+        throw std::invalid_argument("Cannot calculate the factorial of a negative number!");
+    }
+}
