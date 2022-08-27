258f4930fe7602823fbe5d146e4f1042ed13b32b
Yevgeniy Simonov <simonov.yevgeniy@gmail.com>
2022-08-27 14:49:25 +0800

    feat: matplotlib plotting in C++ example with Makefile

---
+++ kalman_filter/test_matplotlib.cpp
@@ -0,0 +1,10 @@
+#include "matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
+
+// g++ test_matplotlib.cpp -std=c++11 -I/usr/include/python3.10 -lpython3.10
+
+int main() {
+    plt::plot({1,3,2,4});
+    plt::show();
+}
