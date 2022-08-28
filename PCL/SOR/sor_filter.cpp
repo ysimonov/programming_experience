b1b4ba057174a2f49e06e072035770675b7c5b45
Yevgeniy Simonov <simonov.yevgeniy@gmail.com>
2022-08-28 23:20:43 +0800

    feat: basic PCL .ply reader

--- PCL/SOR/sor_filter.cpp
+++ PCL/SOR/sor_filter.cpp
@@ -31,21 +31,19 @@ void test_filter()
     // create filtering object
     pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor_filter;
     sor_filter.setInputCloud(cloud);
-    sor_filter.setMeanK(50);
-    sor_filter.setStddevMulThresh(1.0);
+    sor_filter.setMeanK(30);
+    sor_filter.setStddevMulThresh(0.2);
     sor_filter.filter(*cloud_filtered);
 
     std::cerr << "Cloud after filtering: " << std::endl;
     std::cerr << *cloud_filtered << std::endl;
 
     pcl::PCDWriter writer;
-    writer.write<pcl::PointXYZ> ("table_scene_lms400.pcd", *cloud_filtered, false);
 
     sor_filter.setNegative (true);
     sor_filter.filter (*cloud_filtered);
     writer.write<pcl::PointXYZ> ("table_scene_lms400_outliers.pcd", *cloud_filtered, false);
     
-
     //! Segmentation fault when plotting ??
     
     // // Visualize point cloud
@@ -58,14 +56,24 @@ void test_filter()
 
     // while (!viewer->wasStopped()) 
     // {
-    //     // std::this_thread::sleep_for(std::chrono::microseconds(100000));
-
-    //     viewer->spinOnce(100);
-    //     {
-    //         std::lock_guard<std::mutex> lock(mutex_);
-    //         user_data++;
-    //     }
+    //     std::this_thread::sleep_for(std::chrono::microseconds(100000));
+    //     viewer->spin();
     // }
+
+
+    // Visualize point cloud
+    pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+    viewer->setBackgroundColor (0, 0, 0);
+    viewer->addPointCloud<pcl::PointXYZ> (cloud_filtered, "filtered cloud");
+    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "filtered cloud");
+    viewer->addCoordinateSystem (1.0);
+    viewer->initCameraParameters ();
+
+    while (!viewer->wasStopped()) 
+    {
+        std::this_thread::sleep_for(std::chrono::microseconds(100000));
+        viewer->spin();
+    }
 
 }
 
---
+++ PCL/reader/reader.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include <pcl/io/ply_io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+
+std::string PATH = "/home/simon/programming_experience/lidar_data/bunny/data/bun000.ply";
+
+int main()
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PLYReader Reader;
+    Reader.read(PATH, *cloud);
+
+    std::cerr << "Cloud data: " << std::endl;
+    std::cerr << *cloud << std::endl;
+
+    // Visualize point cloud
+    pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+    viewer->setBackgroundColor (0, 0, 0);
+    viewer->addPointCloud<pcl::PointXYZ> (cloud, "sample cloud");
+    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
+    viewer->addCoordinateSystem (1.0);
+    viewer->initCameraParameters ();
+
+    while (!viewer->wasStopped()) 
+    {
+        std::this_thread::sleep_for(std::chrono::microseconds(100000));
+        viewer->spin();
+    }
+
+    return 0;
+}
+
