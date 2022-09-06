09a6b983fa572e1c740bd374352f371a1a83e309
Yevgeniy Simonov <simonov.yevgeniy@gmail.com>
2022-09-06 21:53:05 +0800

    feat: added csv reader header file

--- cpp_sqlite/read_csv.cpp
+++ cpp_sqlite/read_csv.cpp
@@ -7,24 +7,7 @@
 #include <string>
 #include <vector>
 
-/*
- * A class to read data from a csv file.
- */
-class CSVReader
-{
-    std::string fileName;
-    char delimeter;
-    bool header;
-
-  public:
-    // object constructor
-    CSVReader(std::string filename, char delm = ',', bool hdr = true) : fileName(filename), delimeter(delm), header(hdr)
-    {
-    }
-
-    // Function to fetch data from a CSV File
-    std::vector<std::vector<std::string>> getData();
-};
+#include "read_csv.hpp"
 
 /*
  * To detect and ignore byte order mark character
---
+++ cpp_sqlite/read_csv.hpp
@@ -0,0 +1,26 @@
+#ifndef CSV_READER
+#define CSV_READER
+
+#include <string>
+#include <vector>
+
+/*
+ * A class to read data from a csv file.
+ */
+class CSVReader
+{
+    std::string fileName;
+    char delimeter;
+    bool header;
+
+  public:
+    // object constructor
+    CSVReader(std::string filename, char delm = ',', bool hdr = true) : fileName(filename), delimeter(delm), header(hdr)
+    {
+    }
+
+    // Function to fetch data from a CSV File
+    std::vector<std::vector<std::string>> getData();
+};
+
+#endif
