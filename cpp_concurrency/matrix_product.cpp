94c8c27f32ed803f472c7d1fad1d8d696855b4ee
Yevgeniy Simonov <simonov.yevgeniy@gmail.com>
2022-09-16 18:15:49 +0800

    feat: need to figure out why partition sum is not modified inside of boost post

--- cpp_concurrency/matrix_product.cpp
+++ cpp_concurrency/matrix_product.cpp
@@ -10,6 +10,8 @@
 #include <stdexcept>
 #include <string>
 #include <thread>
+#include <utility>  // pair
+#include <vector>
 
 std::recursive_mutex mutex_;
 
@@ -38,21 +40,115 @@ void quick_sum(T *Aik, T *Bkj, T *Cij) {
     (*Cij) += (*Aik) * (*Bkj);
 }
 
+/* apply local summation */
+template <typename T>
+void add_chunks(T ***A, T ***B, std::vector<T> &partition_sum,
+                size_t partition_no, size_t i1, size_t i2, size_t i, size_t j) {
+    // std::cout << "i1: " << i1 << " i2: " << i2 <<
+    // std::endl;
+    size_t current_partition_sum = 0;
+    for (size_t k = i1; k < i2; ++k) {
+        current_partition_sum += (*A)[i][k] * (*B)[k][j];
+    }
+
+    // update partition
+    {
+        std::unique_lock<std::recursive_mutex> lock(mutex_);
+        partition_sum[partition_no] = current_partition_sum;
+        std::cout << "Partition sum: " << current_partition_sum << std::endl;
+    }
+}
+
 /* parallel implementation of matrix multiply */
 template <typename T>
 void parallel_matrix_multiply(T ***A, size_t num_rows_a, size_t num_cols_a,
                               T ***B, size_t num_rows_b, size_t num_cols_b,
                               T ***C) {
-    boost::asio::thread_pool pool(std::thread::hardware_concurrency());
-    for (size_t i = 0; i < num_rows_a; ++i) {
-        for (size_t j = 0; j < num_cols_b; ++j) {
-            (*C)[i][j] = 0;  // initialize result cell to zero
-            // TODO partition matrix product into chunks
-            // Cij = SUM OVER k(Aik * Bkj)
-            // Cijk = Aik * Bkj becomes a tensor product
-            // Then contract tensor Cij = SUM OVER k(Cijk) ???
-            // Not really space efficient ...
+    auto num_partitions = std::thread::hardware_concurrency();
+    auto chunksize = (size_t)((long double)(num_cols_a) / num_partitions);
+
+    if (chunksize <= 1) {
+        sequential_matrix_multiply(A, num_rows_a, num_cols_a, B, num_rows_b,
+                                   num_cols_b, C);
+    } else {  // parallel
+
+        size_t first_idx = 0;
+        size_t last_idx = num_cols_a;
+        std::vector<std::pair<size_t, size_t>> index_bounds;
+        index_bounds.reserve(num_partitions);
+
+        for (auto partition_no = 0; partition_no < num_partitions;
+             ++partition_no) {
+            last_idx = std::min(first_idx + chunksize, num_cols_a);
+            index_bounds.emplace_back(std::make_pair(first_idx, last_idx));
+            first_idx = last_idx;
         }
+
+        boost::asio::thread_pool pool(num_partitions);
+        std::vector<T> partition_sum(num_partitions);
+
+        for (size_t i = 0; i < num_rows_a; ++i) {
+            for (size_t j = 0; j < num_cols_b; ++j) {
+                (*C)[i][j] = 0;  // initialize result cell to zero
+                // TODO partition matrix product into chunks
+                // Cij = SUM OVER k(Aik * Bkj)
+                // Cijk = Aik * Bkj becomes a tensor product
+                // Then contract tensor Cij = SUM OVER k(Cijk) ???
+                // Not really space efficient ...
+
+                // figure out partitions
+
+                // reset values of the partition sum
+                std::fill(partition_sum.begin(), partition_sum.end(), 0);
+
+                for (auto partition_no = 0; partition_no < num_partitions;
+                     ++partition_no) {
+                    auto index_bounds_chunk = index_bounds.at(partition_no);
+                    auto i1 = index_bounds_chunk.first;
+                    auto i2 = index_bounds_chunk.second;
+
+                    // std::cout << "i1: " << i1 << " i2: " << i2 << std::endl;
+
+                    boost::asio::post(pool, [&A, &B, &partition_sum,
+                                             &partition_no, i1, i2, i,
+                                             j]() mutable {
+                        // std::cout << "i1: " << i1 << " i2: " << i2 <<
+                        // std::endl;
+                        size_t current_partition_sum = 0;
+                        for (size_t k = i1; k < i2; ++k) {
+                            current_partition_sum += (*A)[i][k] * (*B)[k][j];
+                        }
+
+                        // update partition
+                        {
+                            std::unique_lock<std::recursive_mutex> lock(mutex_);
+                            partition_sum[partition_no] = current_partition_sum;
+                            std::cout
+                                << "Partition sum: " << current_partition_sum
+                                << std::endl;
+                        }
+                    });
+
+                    // boost::asio::post(pool, add_chunks, A, B, partition_sum,
+                    //                   partition_no, i1, i2, i, j);
+                }
+
+                // wait until all threads in the pool finish execution
+                pool.wait();
+
+                // add result from each of the partitions to C
+                for (const auto &sum : partition_sum) {
+                    std::cout << "sum: " << sum << std::endl;
+                    (*C)[i][j] += sum;
+                }
+
+                // std::cout << "(*C)[" << i << "][" << j << "]: " << (*C)[i][j]
+                //           << std::endl;
+            }
+        }
+
+        // join thread pool
+        pool.join();
     }
 }
 
@@ -114,10 +210,10 @@ void populate_matrix_with_random_values(T ***mat, size_t rows, size_t cols) {
 
 int main() {
     const int NUM_EVAL_RUNS = 3;
-    const size_t NUM_ROWS_A = 1000;
-    const size_t NUM_COLS_A = 1000;
+    const size_t NUM_ROWS_A = 100;  // 1000;
+    const size_t NUM_COLS_A = 100;  // 1000;
     const size_t NUM_ROWS_B = NUM_COLS_A;
-    const size_t NUM_COLS_B = 1000;
+    const size_t NUM_COLS_B = 100;  // 1000;
 
     // ***** General Allocations *****
     long **A = nullptr;
@@ -188,13 +284,16 @@ int main() {
 
     // ***** VERIFICATION *****
     // verify sequential and parallel results
-    for (size_t i = 0; i < NUM_ROWS_A; i++) {
-        for (size_t j = 0; j < NUM_COLS_B; j++) {
-            if (sequential_result[i][j] != parallel_result[i][j]) {
-                printf("ERROR: Result mismatch at row %ld, col %ld!\n", i, j);
-            }
-        }
-    }
+
+    // for (size_t i = 0; i < NUM_ROWS_A; i++) {
+    //     for (size_t j = 0; j < NUM_COLS_B; j++) {
+    //         if (sequential_result[i][j] != parallel_result[i][j]) {
+    //             printf("ERROR: Result mismatch at row %ld, col %ld!\n", i,
+    //             j); printf("Expected result: %ld but received %ld.\n",
+    //                    sequential_result[i][j], parallel_result[i][j]);
+    //         }
+    //     }
+    // }
     printf("Average Sequential Time: %.2f ms\n",
            sequential_time.count() * 1000);
     printf("  Average Parallel Time: %.2f ms\n", parallel_time.count() * 1000);
